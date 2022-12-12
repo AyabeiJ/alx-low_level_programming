dd2dfd7d6ee57259e7f1871e5ad2347f840fbe1a
AyabeiJ <ayabeihilda18@gmail.com>
2022-12-12 03:00:15 -0800

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - main block
- * description: get random number compare it with 5
+ * description: print alphabet in lowercase and then
+ * in uppercase, followed by a new line
  * Return: 0
  */
 int main(void)
 {
 	char c = 'a';
-	
-	while (c <= 'Z')
+
+	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
 	}
-	
+
+	c = 'A';
+
+	while (c <== 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
 	putchar('\n');
 	return (0);
-} 
+}
