c4b6791a365e4914b752fe3d3d3f875f2d84b678
AyabeiJ <ayabeihilda18@gmail.com>
2022-12-12 02:49:04 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * description: get random number compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+	
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	
+	putchar('\n');
+	return (0);
+} 
