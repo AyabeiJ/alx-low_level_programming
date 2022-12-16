1a98de18d986bb3d1e7b996b41044c08f11d8f6f
AyabeiJ <ayabeihilda18@gmail.com>
2022-12-15 17:27:58 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,13 @@
 #include "main.h"
-/**
- * main - print _putchar
- * done by ayabei
- * Return: 0
- */
+#include <unistd.h>
 
-int main(void)
+/**
+ * _putchar - writes the character c to stdout
+ * @: the character to print
+ *
+ * Return: on success 1
+ */
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
