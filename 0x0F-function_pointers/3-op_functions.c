e2c754bed89deab2c25eb30ee7ee5502c5a70bb4
AyabeiJ <ayabeihildaj@gmail.com>
2023-01-21 04:14:17 -0800

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -6,4 +6,25 @@
  * the operation asked by the user
  * @s: the operator passed as argument
  * Return: a pointer to the function corresponding to the
- * operator given as 
+ * operator given as a parameter
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+
+	};
+
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[1].f);
+}
