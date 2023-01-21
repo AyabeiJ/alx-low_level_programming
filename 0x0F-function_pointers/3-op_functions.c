31899924c459754cb1be7e83cc637fec679bb985
AyabeiJ <ayabeihildaj@gmail.com>
2023-01-21 04:04:16 -0800

    3-main.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,9 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - selects the correct function to perform
+ * the operation asked by the user
+ * @s: the operator passed as argument
+ * Return: a pointer to the function corresponding to the
+ * operator given as 
