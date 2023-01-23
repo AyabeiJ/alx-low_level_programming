989001acc6eaa91705e47630b08cfa58b17fceef
AyabeiJ <ayabeihildaj@gmail.com>
2023-01-23 02:18:45 -0800

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,10 +3,10 @@
 
 /**
  * get_op_func - selects the correct function to perform
- * the operation asked by the user
- * @s: the opeator assed as argument
- * Return: a pointer t the function corresponding to the operator
- * given as a parameter
+ * the operation asked by user.
+ * @s: the operator passed as argument.
+ * Return: a pointer to the function corresponding to
+ * the operator given as a parameter
  */
 
 int(*get_op_func(char *s))(int, int)
@@ -16,7 +16,6 @@ int(*get_op_func(char *s))(int, int)
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
-		{"%", op_mod},
 		{NULL, NULL},
 	};
 
